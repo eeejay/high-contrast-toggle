ce5b4670defd58f034531fd9feb925f2675dbcc4
Eitan Isaacson <eitan@monotonous.org>
2022-10-18 14:20:27 -0700

    Insert CSS into content with sensible default colors.
    This is necessary because the browser is probably still set to
    "use system colors" and will use the high contrast theme
    as the default colors. This may not work in non-HCM set websites.

--- background_script.js
+++ background_script.js
@@ -1,13 +1,37 @@
-const TRISTATE = ["high-contrast-only", "never", "always"]
+const CSS_INSERTION_DETAILS = {
+  allFrames: true,
+  file: "/default-colors.css",
+  runAt: "document_start",
+  cssOrigin: "user",
+  matchAboutBlank: true
+};
+
+function insertIntoTab(tab) {
+  return browser.tabs.insertCSS(tab.id, CSS_INSERTION_DETAILS).catch(console.warn.bind(console));
+}
+
+function removeFromTab(tab) {
+  return browser.tabs.removeCSS(tab.id, CSS_INSERTION_DETAILS).catch(console.warn.bind(console));
+}
+
+async function removeCSS() {
+  browser.tabs.onUpdated.removeListener(insertIntoTab);
+  let allTabs = await browser.tabs.query({});
+  await Promise.all(allTabs.map(removeFromTab));
+}
+
+async function insertCSS() {
+  browser.tabs.onUpdated.addListener(insertIntoTab);
+  let allTabs = await browser.tabs.query({});
+  await Promise.all(allTabs.map(insertIntoTab));
+}
+
+const TOGGLESTATES = [/* "high-contrast-only", */ "never", "always"]
 const BADGE_TEXT = {"high-contrast-only": "Auto", "never": "Off", "always": "On"};
+const CSS_OPERATION = {"high-contrast-only": removeCSS, "never": insertCSS, "always": removeCSS};
 
 async function setActionInfo(value) {
-  if (value == undefined) {
-    let result = await browser.browserSettings.overrideDocumentColors.get({});
-    value = result.value;
-    await browser.browserAction.setBadgeBackgroundColor({ color: "#000" });
-    await browser.browserAction.setBadgeTextColor({ color: "#ff0" });
-  }
+
   let badgeText = BADGE_TEXT[value];
   await browser.browserAction.setBadgeText({ text: badgeText == "Auto" ? "" : badgeText });
   await browser.browserAction.setTitle({ title: `High Contrast: ${badgeText}` });
@@ -15,15 +39,25 @@ async function setActionInfo(value) {
 
 async function toggleHighContrast() {
   let { value } = await browser.browserSettings.overrideDocumentColors.get({});
-  let nextValue = TRISTATE[(TRISTATE.indexOf(value) + 1) % TRISTATE.length];
+  let nextValue = TOGGLESTATES[(TOGGLESTATES.indexOf(value) + 1) % TOGGLESTATES.length];
   let success = await browser.browserSettings.overrideDocumentColors.set({ value: nextValue });
   if (success) {
+    CSS_OPERATION[nextValue]();
     setActionInfo(nextValue);
   }
 }
 
-browser.browserAction.onClicked.addListener(() => {
-  toggleHighContrast();
-});
 
-setActionInfo();
+async function init() {
+  browser.browserAction.onClicked.addListener(() => {
+    toggleHighContrast();
+  });
+
+  await browser.browserAction.setBadgeBackgroundColor({ color: "#000" });
+  await browser.browserAction.setBadgeTextColor({ color: "#ff0" });
+  let { value } = await browser.browserSettings.overrideDocumentColors.get({});
+  CSS_OPERATION[value]();
+  setActionInfo(value);
+}
+
+init();
