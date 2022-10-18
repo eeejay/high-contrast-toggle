354dd4556d8e3d1d31fcb85f924f36f566d9357b
Eitan Isaacson <eitan@monotonous.org>
2022-10-18 11:19:30 -0700

    Toggle through all override modes

--- background_script.js
+++ background_script.js
@@ -1,26 +1,24 @@
-async function setActionInfo(toggled) {
-  if (toggled == undefined) {
-    let { levelOfControl } = await browser.browserSettings.overrideDocumentColors.get({});
-    toggled = levelOfControl == "controlled_by_this_extension";
+const TRISTATE = ["high-contrast-only", "never", "always"]
+const BADGE_TEXT = {"high-contrast-only": "Auto", "never": "Off", "always": "On"};
+
+async function setActionInfo(value) {
+  if (value == undefined) {
+    let result = await browser.browserSettings.overrideDocumentColors.get({});
+    value = result.value;
+    await browser.browserAction.setBadgeBackgroundColor({ color: "#000" });
+    await browser.browserAction.setBadgeTextColor({ color: "#ff0" });
   }
-  if (toggled) {
-    await browser.browserAction.setBadgeText({ text: "ON" });
-    await browser.browserAction.setTitle({ title: "High Contrast On" });
-  } else {
-    await browser.browserAction.setBadgeText({ text: "" });
-    await browser.browserAction.setTitle({ title: "High Contrast Off" });
-  }
+  let badgeText = BADGE_TEXT[value];
+  await browser.browserAction.setBadgeText({ text: badgeText == "Auto" ? "" : badgeText });
+  await browser.browserAction.setTitle({ title: `High Contrast: ${badgeText}` });
 }
 
 async function toggleHighContrast() {
-  let { levelOfControl } = await browser.browserSettings.overrideDocumentColors.get({});
-  let success = false;
-  if (levelOfControl == "controlled_by_this_extension") {
-    success = await browser.browserSettings.overrideDocumentColors.clear({});
-    setActionInfo(!success);
-  } else {
-    success = await browser.browserSettings.overrideDocumentColors.set({ value: "always" });
-    setActionInfo(success);
+  let { value } = await browser.browserSettings.overrideDocumentColors.get({});
+  let nextValue = TRISTATE[(TRISTATE.indexOf(value) + 1) % TRISTATE.length];
+  let success = await browser.browserSettings.overrideDocumentColors.set({ value: nextValue });
+  if (success) {
+    setActionInfo(nextValue);
   }
 }
 
